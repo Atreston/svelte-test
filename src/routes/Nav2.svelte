424e5812b8e62b2965ef81401afdd39710321262
Atreston <33429783+Atreston@users.noreply.github.com>
2022-05-09 11:47:38 -0500

    Add files via upload

---
+++ src/routes/Nav2.svelte
@@ -0,0 +1,22 @@
+<div class="wrapper">
+    <div class="bg-gray-200">
+        <div class="menu flex justify-end">
+            <button class="bg-yellow-600 text-white rounded font-bold py-2 px-4"><a href="#">Doc</a></button>
+            <button class="bg-yellow-600 text-white rounded font-bold py-2 px-4"><a href="#">Blog</a></button>
+            <button class="bg-yellow-600 text-white rounded font-bold py-2 px-4"><a href="#">FAQ</a></button>
+            <button class="bg-yellow-600 text-white rounded font-bold py-2 px-4"><a href="#">About</a></button>
+        </div>
+    </div>
+</div>
+
+<div class="wrapper">
+    <ul class="menu bg-green-100 flex justify-end">
+        <li class="mx-8"><a href="https://google.com">Doc</a></li>
+        <li class="mx-8">Blog</li>
+        <li class="mx-8">FAQ</li>
+        <li class="mx-8">About</li>
+    </ul>
+</div>
+
+<style>
+</style>
